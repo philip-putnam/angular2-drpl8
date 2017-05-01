879b4a88143568c3b4caf7d4d8de0fcf639e33b7
Philip Putnam <putnamp989@gmail.com>
2017-05-01 14:10:01 -0700

    add home component, router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppService } from './services/app.service';
 import { BlogService } from './services/blog.service';
 import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'blog', component: BlogComponent },
+  { path: '', component: HomeComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent
+    BlogComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AppService, BlogService],
   bootstrap: [AppComponent]
